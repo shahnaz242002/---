ea28d4c1a0a8c1fefc26f71670755af230dafafd
shahn <shahn@DESKTOP-P54G1G2>
2023-03-16 15:56:07 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+Console.OutputEncoding=Encoding.UTF8;
+//Напишите часть программы регистрации аккаунта.
+//Пользователь вводит пароль и затем повторяет его.
+//Если пароли совпадают выведите на консоль сообщение о успешной регистрации. Если нет - сообщение об ошибке.
+Console.WriteLine("Введите пароль:");
+int password = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите пароль повторно:");
+int otherPassword = Convert.ToInt32(Console.ReadLine());
+if (password == otherPassword)
+{
+    Console.WriteLine("Correct");
+}
+if (password != otherPassword)
+{
+    Console.WriteLine("Incorrect");
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+Console.OutputEncoding=Encoding.UTF8;
+string name = "xurma";
+Console.WriteLine("Введите количество товарa");
+int count = Convert.ToInt32(Console.ReadLine());
+int price = 20;
+string aksia = "yes";
+double result = price * count;
+if(count>=5)
+{
+    result = result * 0.9;
+}
+if(aksia=="yes")
+{
+    result = result * 0.85;
+}
+Console.WriteLine(result);
